6005bc83388203be4aaa7d80161dbd1a1b4660ce
antoine <antoine.gillot60@gmail.com>
2018-06-05 09:26:40 +0200

    remove comments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,19 +25,4 @@
     </div>
     <div class="content-ajax">
     </div>
-
-    {{--@foreach($recettes->sortByDesc('created_at') as $recette)--}}
-    {{--<div class="col-xs-4">--}}
-    {{--<div class="panel panel-default">--}}
-    {{--<div class="panel-heading">{{$recette->name}}</div>--}}
-    {{--<div class="panel-body"><img src="{{$recette->image}}"/></div>--}}
-    {{--<div class="panel-footer"><a href="recettes/{{$recette->id}}">Détail</a>--}}
-    {{--<div class="right">{!! number_format($recette->averageRating, 1) !!} / 5--}}
-    {{--({{ $recette->userSumRating }} votes)--}}
-    {{--</div>--}}
-    {{--<div>By {{ $recette->user->name }}</div>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--@endforeach--}}
 @endsection
